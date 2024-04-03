a02d7ca80e20c8d100dcf9aba28d7d31245b752b
Ben Ritter <benritter@Bens-MacBook-Air.local>
2024-04-03 10:53:30 -0700

    Functioning MVC version

--- game.py
+++ game.py
@@ -8,46 +8,23 @@ class Game:
 
     def __init__(self):
         self.model = Model()
-        self.controller = Controller(self.model)
-        self.view = View(self.controller)
+        self.view = View()
+        self.controller = Controller(self.model, self.view)
         self.loop()
 
-    def set_state(self, state):
-        if state == "showing_winner":
-            self.view.set_state("showing_winner")
-        if state == "showing_tie":
-            self.view.set_state("showing_tie")
     def loop(self):
-        # Game states
-        # * Prompting user to move
-        # * Showing results of move
-        # * Showing hint
-        # * Prompting user to play again
-        # * Showing results of finished game
-        # * Display game result (win or tie)
-        winner_or_tie = False
-        while not winner_or_tie:
+        state = "PLAY"
+        while state != "WIN" or state != "TIE":
             os.system("clear")
-            self.view.set_state("showing_board")
-            self.view.set_state("prompting_user")
-            res = self.controller.get_move()
-            if not res:
-                os.system("clear")
-                self.view.set_state("showing_help")
-                self.view.set_state("prompting_user")
-                res = self.controller.get_move()
-            else:
-                self.controller.update_board(res)
-                # Here we can implement controller states: play, win, tie
-                result = self.controller.check_for_win_or_tie()
-                if result:
-                    winner_or_tie = result
-                    os.system("clear")
-                    self.view.set_state("showing_board")
-                if result == "WIN":
-                    self.view.set_state("showing_winner")
-                    break
-                elif result == "TIE":
-                    self.view.set_state("showing_tie")
-                    break
-                self.controller.switch_players()
+            if state == "PLAY":
+                state = self.controller.get_move()
+            elif state == "INVALID_INPUT":
+                state = self.controller.show_help()
+            elif state == "INVALID_MOVE":
+                state = self.controller.get_move()
+            elif state == "WIN":
+                state = self.controller.show_win()
+            elif state == "TIE":
+                state = self.controller.show_tie()
+            elif state == "EXIT":
+                exit()
